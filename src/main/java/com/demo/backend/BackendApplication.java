e87822df8b027013ae59c5d51515f314d24f500a
Wojciech Wojcik <wojciech.wojcik@harman.com>
2019-10-22 17:07:58 +0100

    add put

--- src/main/java/com/demo/backend/BackendApplication.java
+++ src/main/java/com/demo/backend/BackendApplication.java
@@ -6,11 +6,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -23,21 +28,33 @@ public class BackendApplication {
     @RestController
     @RequestMapping("api")
     public static class RocketController {
+
+        private List<Rocket> rockets = new ArrayList<>();
+
         @GetMapping
         @ResponseBody
-        public RocketResponse get() {
+        public List<Rocket> get() {
+            log.info("Getting all rockets");
+            return rockets;
+        }
+
+        @PutMapping
+        @ResponseBody
+        public Rocket put() throws UnknownHostException {
             log.info("Building new rocket");
-            return RocketResponse.builder()
-                    .host("localhost")
+            Rocket rocket = Rocket.builder()
+                    .host(InetAddress.getLocalHost().getHostName())
                     .timestamp(LocalDateTime.now())
                     .build();
+            rockets.add(rocket);
+            return rocket;
         }
     }
 
     @Data
     @Builder
-    public static class RocketResponse {
-        String host;
-        LocalDateTime timestamp;
+    public static class Rocket {
+        private String host;
+        private LocalDateTime timestamp;
     }
 }
